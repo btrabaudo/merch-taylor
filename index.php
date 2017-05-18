3af10a1b4c6d22322ad5e612eaf32e63ed73e06e
Billy Trabaudo <btrabaudo@gmail.com>
2017-05-18 10:34:19 -0600

    index.html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Merchant-Taylor Home Page</title>
+
+        <!-- custom CSS stylesheet -->
+        <link rel="stylesheet" href="../merch-taylor/css/style.css" type="text/css" />
+
+    </head>
+
+    <body>
+        <h1>Merchant-Taylor International, Inc.</h1>
+    </body>
+
+</html>
