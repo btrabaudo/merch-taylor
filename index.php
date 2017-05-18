f0a7d6512d5e8136f1d9593736407333e8167b7c
Billy Trabaudo <btrabaudo@gmail.com>
2017-05-18 12:00:37 -0600

    adding content

--- index.php
+++ index.php
@@ -5,13 +5,32 @@
         <meta charset="utf-8">
         <title>Merchant-Taylor Home Page</title>
 
+
         <!-- custom CSS stylesheet -->
         <link rel="stylesheet" href="../merch-taylor/css/style.css" type="text/css" />
+
+
 
     </head>
 
     <body>
+
+
         <h1>Merchant-Taylor International, Inc.</h1>
+
+        <h2>Biomedical Regulatory & New Product</h2>
+        <h2>Development Consulting</h2>
+
+    <!-- nav bar list-->
+
+        <ul>
+            <li> <a href="index.php">Welcome</a></li>
+            <li> <a href="about.php">About Page</a> </li>
+            <li> <a href="consulting.php">Consulting Services</a></li>
+            <li> <a href="clientlist.php">Partial Client List</a></li>
+        </ul>
+
+
     </body>
 
 </html>
