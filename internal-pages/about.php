e46f0c2f6fc7ba94b33531b2616b6eecf4b4f8a2
Billy Trabaudo <btrabaudo@gmail.com>
2017-05-19 13:51:34 -0600

    added nav bar to other sites

---
+++ internal-pages/about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Merchant-Taylor About</title>
+
+
+
+</head>
+
+<body>
+<div class = "container">
+
+</div>
+
+<!-- nav bar list-->
+<div class = "container1">
+    <ul>
+        <img src="/images/ltlogo.jpg">
+        <li> <a href="/index.php">Welcome</a></li>
+        <li> <a href="about.php">About Page</a> </li>
+        <li> <a href="consulting.php">Consulting Services</a></li>
+        <li> <a href="clientList.php">Partial Client List</a></li>
+        <li> <a href="principles.php">MTI Principals</a></li>
+        <li> <a href="associates.php">MTI Associates</a></li>
+        <li> <a href="contact.php">Contact Us</a></li>
+    </ul>
+</div>
+
+</body>
+
+</html>
